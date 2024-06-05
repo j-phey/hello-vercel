18a76da0b1227923f43995f80b9aa1a94044dc18
Jon Phey <jonvercel@jons-mbp.lan>
2024-06-05 15:18:19 +1000

    added content to route.js

---
+++ app/api/hello/route.js
@@ -0,0 +1,10 @@
+export const runtime = 'nodejs';
+ 
+// To stream responses in Next.js, you must use Route Handlers in the App Router, even if the rest of your app uses the Pages Router.
+ 
+export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+ 
+export function GET(request) {
+    console.log(`VERCEL_REGION: ${process.env.VERCEL_REGION}`)
+    return new Response(`Hello from ${process.env.VERCEL_REGION}`)
+}
